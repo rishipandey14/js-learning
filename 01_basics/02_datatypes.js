443412ba53944378b8f52e7873419da9598e359e
Rishi <hopesalive1403@gmail.com>
2024-10-25 12:53:37 +0530

    completed vi no - 5

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";   // treat all js code as newer version
+
+// alert(3 + 3)    we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Rishi")
+
+
+let name = "rishi"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+
+
+// number => 2^53
+// bigint
+// string =>  ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof age)
+console.log(typeof "rishi")
+
