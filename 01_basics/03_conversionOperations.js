f5874103f4727e55324043657565352c3d795d1a
Rishi <hopesalive1403@gmail.com>
2024-10-28 13:11:27 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+// let score = "rishi"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "rishi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn )
+
+// 1 => true;  0 => false
+//  "" => false;
+// "rishi" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
